34c80715d31d3ebd75ceaa5e2838c104a429e0a9
gamingar-tony <gamingar@outlook.com>
2025-05-21 10:05:07 -0500

    Updated views.py

--- ministry_scheduler/core/views.py
+++ ministry_scheduler/core/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    from django.contrib.auth.forms import AuthenticationForm
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
+
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('/')
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/login/')
