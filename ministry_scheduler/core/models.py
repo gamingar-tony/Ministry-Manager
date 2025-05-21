64ce3ede13abc69b9f1ab5a792bfdd2c670f96d4
gamingar-tony <gamingar@outlook.com>
2025-05-21 10:16:56 -0500

    Updated models.py

--- ministry_scheduler/core/models.py
+++ ministry_scheduler/core/models.py
@@ -9,3 +9,12 @@ class User(AbstractUser):
     ]
 
     role = models.CharField(max_length = 10, choices = ROLE_CHOICES, default = 'volunteer')
+
+class ScheduleEntry(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    role = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role} on {self.date} at {self.time}"
